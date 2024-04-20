8c09da2e8822a1204e54b258e07ed36e32568155
3nf4n7 <myfirsttwink1@gmail.com>
2024-04-21 00:02:51 +0300

    adaptive

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -5,13 +5,14 @@ import cn from 'classnames';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../store/store';
 import { getProfile, userActions } from '../../store/user.slice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export function Layout() {
 	const navigate = useNavigate();
 	const dispatch = useDispatch<AppDispatch>();
 	const profile = useSelector((s: RootState) => s.user.profile);
 	const items = useSelector((s: RootState) => s.cart.items);
+	const [enableSidebar, setEnableSidebar] = useState(true);
 
 	useEffect(() => {
 		dispatch(getProfile());
@@ -22,9 +23,20 @@ export function Layout() {
 		navigate('/auth/login');
 	};
 
+	const showSidebar = () => {
+		setEnableSidebar(!enableSidebar);	
+	};
+
 	return (
 		<div className={styles['layout']}>
-			<div className={styles['sidebar']}>
+			<button className={cn(styles['sidebar-icon'], {
+				[styles['sidebar-icon-active']]: enableSidebar
+			})} onClick={showSidebar}>
+				<img src="/sidebar-icon.svg" alt="" />
+			</button>
+			<div className={cn(styles['sidebar'], {
+				[styles['sidebar-active']]: enableSidebar
+			})}>
 				<div className={styles['user']}>
 					<div className={styles.avatar}>
 						<img src="/avatar.svg" alt="" />
